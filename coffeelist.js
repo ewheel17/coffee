5e078a2d8630a758c7ab33924d5658886988ee75
Sammy Straw <SStraw@carbonite.com>
2018-04-05 15:56:45 -0600

    SLRCo and Beans & Brew added

--- coffeelist.js
+++ coffeelist.js
@@ -635,128 +635,573 @@ var ibisVienna = {
     decaf: false,
 };
 
-var coffeeName = {
-    name: "",
-    brand: "",
-    roast: "",
+
+// SALT LAKE ROASTING CO Coffees
+
+var slrAM = {
+    name: "A.M. Blend",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
     profile: [""],
-    origin: [""],
+    origin: ["Central America", "South America"],
     decaf: false,
 };
 
-var coffeeName = {
-    name: "",
-    brand: "",
-    roast: "",
-    profile: [""],
-    origin: [""],
+var slrAfricanQueen = {
+    name: "African Queen",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["sweet", "fragrant"],
+    origin: ["Africa"],
     decaf: false,
 };
 
-var coffeeName = {
-    name: "",
-    brand: "",
-    roast: "",
-    profile: [""],
-    origin: [""],
+var slrAlpenglow = {
+    name: "Alpenglow Blend (Organic & Fair Trade)",
+    brand: "Salt Lake Roasting Co",
+    roast: "Medium",
+    profile: ["full-bodied"],
+    origin: ["Africa", "South America", "Central America"],
     decaf: false,
 };
 
-var coffeeName = {
-    name: "",
-    brand: "",
-    roast: "",
-    profile: [""],
-    origin: [""],
+var slrBrazil = {
+    name: "Brazil",
+    brand: "Salt Lake Roasting Co",
+    roast: "Medium",
+    profile: ["earthy", "sweet", "full-bodied"],
+    origin: ["Brazil"],
     decaf: false,
 };
 
-var coffeeName = {
-    name: "",
-    brand: "",
-    roast: "",
-    profile: [""],
-    origin: [""],
+var slrBrazilO = {
+    name: "Brazil (Organic)",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["fruity", "cocoa", "apple", "caramel", "sweet"],
+    origin: ["Brazil"],
     decaf: false,
 };
 
-var coffeeName = {
-    name: "",
-    brand: "",
-    roast: "",
-    profile: [""],
-    origin: [""],
+var slrCelebesKolassie = {
+    name: "Celebes Kolassie / Sulawesi",
+    brand: "Salt Lake Roasting Co",
+    roast: "Medium",
+    profile: ["full-bodied"],
+    origin: ["Indonesia"],
     decaf: false,
 };
-var coffeeName = {
-    name: "",
-    brand: "",
-    roast: "",
-    profile: [""],
-    origin: [""],
-    decaf: false,
-};
-
-var coffeeName = {
-    name: "",
-    brand: "",
-    roast: "",
-    profile: [""],
-    origin: [""],
+var slrColombia = {
+    name: "Colombia",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["bright"],
+    origin: ["Colombia"],
     decaf: false,
 };
 
-var coffeeName = {
-    name: "",
-    brand: "",
-    roast: "",
-    profile: [""],
-    origin: [""],
+var slrColombiaFTO = {
+    name: "Colombia Fair Trade Organic Cauca Amucc Ecotact",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["lemon", "cherry", "caramel", "apple"],
+    origin: ["Colombia"],
     decaf: false,
 };
 
-var coffeeName = {
-    name: "",
-    brand: "",
-    roast: "",
-    profile: [""],
-    origin: [""],
+var slrColombiaDecaf = {
+    name: "Colombia Natural Decaf",
+    brand: "Salt Lake Roasting Co",
+    roast: "Medium",
+    profile: ["floral", "rich"],
+    origin: ["Colombia"],
+    decaf: true,
+};
+
+var slrCostaRica = {
+    name: "Costa Rica",
+    brand: "Salt Lake Roasting Co",
+    roast: "Medium",
+    profile: ["tart", "full-bodied"],
+    origin: ["Costa Rica"],
     decaf: false,
 };
 
-var coffeeName = {
-    name: "",
-    brand: "",
-    roast: "",
-    profile: [""],
-    origin: [""],
+var slrCostaRicaO = {
+    name: "Costa Rica - La Amistad Organic",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["maple", "sweet", "cherry", "butter"],
+    origin: ["Costa Rica"],
     decaf: false,
 };
 
-var coffeeName = {
-    name: "",
-    brand: "",
-    roast: "",
-    profile: [""],
-    origin: [""],
+var slrElSalv = {
+    name: "El Salvador Honey Pacamara - Finca Leticia",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["chocolate", "malt", "citrus", "fruity", "honey", "crisp"],
+    origin: ["El Salvador"],
     decaf: false,
 };
 
-var coffeeName = {
-    name: "",
-    brand: "",
-    roast: "",
-    profile: [""],
-    origin: [""],
+var slrEspresso = {
+    name: "Espresso",
+    brand: "Salt Lake Roasting Co",
+    roast: "Espresso",
+    profile: ["full-bodied", "oily"],
+    origin: ["Unknown"],
     decaf: false,
 };
 
-var coffeeName = {
-    name: "",
-    brand: "",
-    roast: "",
+var slrEspressoDecaf = {
+    name: "Espresso Decaf KVW",
+    brand: "Salt Lake Roasting Co",
+    roast: "Espresso",
+    profile: ["dark", "oily"],
+    origin: ["Unknown"],
+    decaf: true,
+};
+
+var slrEspressoO = {
+    name: "Espresso Organic (Formerly Viennese)",
+    brand: "Salt Lake Roasting Co",
+    roast: "Espresso",
+    profile: ["dark"],
+    origin: ["Unknown"],
+    decaf: false,
+};
+
+var slrEthiopiaMoccaDecaf = {
+    name: "Ethiopia (Mocca) Decaf Fair Trade Organic",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["unique", "wine"],
+    origin: ["Ethiopia"],
+    decaf: true,
+};
+
+var slrEthiopiaLongberry = {
+    name: "Ethiopia Longberry Harrar",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["wine", "unique"],
+    origin: ["Ethiopia"],
+    decaf: false,
+};
+
+var slrEthiopiaYirgacheffeO = {
+    name: "Ethiopia Natural Yirgacheffe (Organic)",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["fruity", "full-bodied"],
+    origin: ["Ethiopia"],
+    decaf: false,
+};
+
+var slrEthiopiaYirgacheffe = {
+    name: "Ethiopia Yirgacheffe",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["sweet", "bright", "delicate"],
+    origin: ["Ethiopia"],
+    decaf: false,
+};
+
+var slrFarEast = {
+    name: "Far East Rim",
+    brand: "Salt Lake Roasting Co",
+    roast: "Dark",
+    profile: ["earthy", "full-bodied", "dark"],
+    origin: ["Unknown"],
+    decaf: false,
+};
+
+var slrFrenchRoast = {
+    name: "French Roast",
+    brand: "Salt Lake Roasting Co",
+    roast: ["French", "Dark"],
+    profile: ["caramel", "dark"],
+    origin: ["Unknown"],
+    decaf: false,
+};
+
+var slrGuatemalaAntigua = {
+    name: "Guatemala Antigua Bella Carmona",
+    brand: "Salt Lake Roasting Co",
+    roast: "Medium",
+    profile: ["full-bodied", "crisp", "spicy"],
+    origin: ["Guatemala"],
+    decaf: false,
+};
+
+var slrGuatemalaO = {
+    name: "Guatemala Organic",
+    brand: "Salt Lake Roasting Co",
+    roast: "Medium",
+    profile: ["wine", "complex"],
+    origin: ["Guatemala"],
+    decaf: false,
+};
+
+var slrHawaiianSunrise = {
+    name: "Hawaiian Sunrise",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["kona", "delicate", "sweet"],
+    origin: ["Hawaii"],
+    decaf: false,
+};
+
+var slrHonduras = {
+    name: "Honduras Fair Trade Organic",
+    brand: "Salt Lake Roasting Co",
+    roast: "Medium",
+    profile: ["acidic"],
+    origin: ["Honduras"],
+    decaf: false,
+};
+
+var slrHouse = {
+    name: "House Blend",
+    brand: "Salt Lake Roasting Co",
+    roast: "Medium",
+    profile: ["rich", "full-bodied"],
+    origin: ["Central America", "Indonesia", "Africa"],
+    decaf: false,
+};
+
+var slrHouseD = {
+    name: "House Blend Decaf",
+    brand: "Salt Lake Roasting Co",
+    roast: "Medium",
+    profile: ["rich", "full-bodied"],
+    origin: ["Central America", "Indonesia", "Africa"],
+    decaf: true,
+};
+
+var slrIndiaMonsoon = {
+    name: "India Monsoon Malabar",
+    brand: "Salt Lake Roasting Co",
+    roast: "Dark",
+    profile: ["earthy", "dark", "musty"],
+    origin: ["India"],
+    decaf: false,
+};
+
+var slrJamaicaBlueMtn = {
+    name: "Jamaica Blue Mountain",
+    brand: "Salt Lake Roasting Co",
+    roast: "Dark",
     profile: [""],
-    origin: [""],
+    origin: ["Jamaica"],
+    decaf: false,
+};
+
+var slrJamaicaSunrise = {
+    name: "Jamaica Sunrise",
+    brand: "Salt Lake Roasting Co",
+    roast: "Dark",
+    profile: [""],
+    origin: ["Jamaica"],
+    decaf: false,
+};
+
+var slrJava = {
+    name: "Java",
+    brand: "Salt Lake Roasting Co",
+    roast: "Medium",
+    profile: ["full-bodied", "thick", "smooth"],
+    origin: ["Indonesia"],
+    decaf: false,
+};
+
+var slrKenya = {
+    name: "Kenya",
+    brand: "Salt Lake Roasting Co",
+    roast: "Dark",
+    profile: ["wine", "acidic", "full-bodied"],
+    origin: ["Kenya"],
+    decaf: false,
+};
+
+var slrKenyaNyeri = {
+    name: "Kenya Nyeri Barichu Gatomboya",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["blackberry", "jasmine", "grapefruit", "sun-dried tomato"],
+    origin: ["Kenya"],
+    decaf: false,
+};
+
+var slrLateNight = {
+    name: "Late Night",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["half decaf"],
+    origin: ["Unknown"],
+    decaf: true,
+};
+
+var slrMexicoD = {
+    name: "Mexico Decaf (Organic Swiss Water Process)",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["snappy", "aromatic"],
+    origin: ["Mexico"],
+    decaf: true,
+};
+
+var slrMexicoO = {
+    name: "Mexico Organic",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["snappy", "dry", "aromatic"],
+    origin: ["Mexico"],
+    decaf: false,
+};
+
+var slrMochaJavaD = {
+    name: "Mocha / Java KVW Decaf",
+    brand: "Salt Lake Roasting Co",
+    roast: "Dark",
+    profile: ["full-bodied", "wild", "thick"],
+    origin: ["Indonesia", "Africa"],
+    decaf: true,
+};
+
+var slrMochaJava = {
+    name: "Mocha/Java",
+    brand: "Salt Lake Roasting Co",
+    roast: "Dark",
+    profile: ["full-bodied", "wild", "thick"],
+    origin: ["Indonesia", "Africa"],
+    decaf: false,
+};
+
+var slrMorningThunder = {
+    name: "Morning Thunder",
+    brand: "Salt Lake Roasting Co",
+    roast: "Medium",
+    profile: ["elegant", "rich", "full-bodied"],
+    origin: ["Sulawesi"],
+    decaf: false,
+};
+
+var slrNewGuinea = {
+    name: "New Guinea",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["clean", "bright", "rounded"],
+    origin: ["Indonesia"],
+    decaf: false,
+};
+
+var slrNicaraguaFTO = {
+    name: "Nicaragua (Organic & Fair Trade)",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["clean", "lively", "refreshing"],
+    origin: ["Nicaragua"],
+    decaf: false,
+};
+
+var slrOldDutch = {
+    name: "Old Dutch",
+    brand: "Salt Lake Roasting Co",
+    roast: "Dark",
+    profile: ["earthy", "full-bodied"],
+    origin: ["Unknown"],
+    decaf: false,
+};
+
+var slrPanama = {
+    name: "Panama Maunier",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["aromatic", "floral", "clean", "full-bodied"],
+    origin: ["Panama"],
+    decaf: false,
+};
+
+var slrPeruFTO = {
+    name: "Peru Norandino (Organic & Fair Trade)",
+    brand: "Salt Lake Roasting Co",
+    roast: "Light",
+    profile: ["bright", "clean"],
+    origin: ["South America", "Peru"],
+    decaf: false,
+};
+
+var slrSilverfork = {
+    name: "Silverfork Blend",
+    brand: "Salt Lake Roasting Co",
+    roast: "Dark",
+    profile: ["dark", "thick"],
+    origin: ["Unknown"],
+    decaf: false,
+};
+
+var slrSumatra = {
+    name: "Sumatra",
+    brand: "Salt Lake Roasting Co",
+    roast: "Dark",
+    profile: ["earthy", "chocolate", "syrup"],
+    origin: ["Indonesia"],
+    decaf: false,
+};
+
+var slrSumatraO = {
+    name: "Sumatra (Organic)",
+    brand: "Salt Lake Roasting Co",
+    roast: "Dark",
+    profile: ["earthy", "chocolate", "syrup"],
+    origin: ["Indonesia"],
+    decaf: false,
+};
+
+var slrSumatraO = {
+    name: "Sumatra (Organic)",
+    brand: "Salt Lake Roasting Co",
+    roast: "Dark",
+    profile: ["earthy", "chocolate", "syrup"],
+    origin: ["Indonesia"],
+    decaf: true,
+};
+
+// BEANS & BREW
+var bbHouse = {
+    name: "House Blend",
+    brand: "Beans & Brew (High Altitude Roasting)",
+    roast: "Medium",
+    profile: ["smooth", "full-bodied", "tangy"],
+    origin: ["Unknown"],
+    decaf: false,
+};
+
+var bbHouseD = {
+    name: "Decaf House Blend",
+    brand: "Beans & Brew (High Altitude Roasting)",
+    roast: "Medium",
+    profile: ["smooth", "full-bodied", "tangy"],
+    origin: ["Unknown"],
+    decaf: true,
+};
+
+var bbBreakfast = {
+    name: "Breakfast Blend",
+    brand: "Beans & Brew (High Altitude Roasting)",
+    roast: "Medium",
+    profile: ["aromatic", "rich", "snappy"],
+    origin: ["Americas"],
+    decaf: false,
+};
+
+var bbColombia = {
+    name: "Colombia Santander Fair Trade Organic",
+    brand: "Beans & Brew (High Altitude Roasting)",
+    roast: "Dark",
+    profile: ["full-bodied", "brown sugar", "lemon", "crisp", "clean"],
+    origin: ["Colombia"],
+    decaf: false,
+};
+
+var bbEspressoFritalia = {
+    name: "Espresso Fritalia",
+    brand: "Beans & Brew (High Altitude Roasting)",
+    roast: "Dark",
+    profile: ["intense", "dark", "rich"],
+    origin: ["Unknown"],
+    decaf: false,
+};
+
+var bbFrenchRoast = {
+    name: "French Roast",
+    brand: "Beans & Brew (High Altitude Roasting)",
+    roast: "Dark",
+    profile: ["dark", "syrup", "rich"],
+    origin: ["Unknown"],
+    decaf: false,
+};
+
+var bbFrenchRoastD = {
+    name: "French Roast Decaf",
+    brand: "Beans & Brew (High Altitude Roasting)",
+    roast: "Dark",
+    profile: ["dark", "syrup", "rich"],
+    origin: ["Unknown"],
+    decaf: true,
+};
+
+var bbFrenchRoastFTO = {
+    name: "French Roast Fair Trade Organic",
+    brand: "Beans & Brew (High Altitude Roasting)",
+    roast: "Dark",
+    profile: ["dark", "syrup", "rich"],
+    origin: ["Sumatra", "Central America"],
+    decaf: false,
+};
+
+var bbGuatemalaFTO = {
+    name: "Guatemala Huehuetenango Fair Trade Organic",
+    brand: "Beans & Brew (High Altitude Roasting)",
+    roast: "Medium",
+    profile: [""],
+    origin: ["Guatemala"],
+    decaf: false,
+};
+
+var bbItalian = {
+    name: "Italian Roast",
+    brand: "Beans & Brew (High Altitude Roasting)",
+    roast: "Dark",
+    profile: ["dark", "caramel"],
+    origin: ["Unknown"],
+    decaf: false,
+};
+
+var bbKenya = {
+    name: "Kenya Barichu Gatomboya",
+    brand: "Beans & Brew (High Altitude Roasting)",
+    roast: "Light",
+    profile: ["grapefruit", "jasmine", "blackberry", "crisp", "clean", "bright"],
+    origin: ["Kenya"],
+    decaf: false,
+};
+
+var bbRockyMtn = {
+    name: "Rocky Mountain Blend",
+    brand: "Beans & Brew (High Altitude Roasting)",
+    roast: "Dark",
+    profile: ["full-bodied"],
+    origin: ["Indonesia"],
+    decaf: false,
+};
+
+var bbSulawesi = {
+    name: "Sulawesi Jaya Peaberry",
+    brand: "Beans & Brew (High Altitude Roasting)",
+    roast: "Light",
+    profile: ["full-bodied"],
+    origin: ["Sulawesi", "Indonesia"],
+    decaf: false,
+};
+
+var bbSumatra = {
+    name: "Sumatra Mandheling",
+    brand: "Beans & Brew (High Altitude Roasting)",
+    roast: "Medium",
+    profile: ["rich", "full-bodied", "smooth"],
+    origin: ["Sumatra"],
+    decaf: false,
+};
+
+var bbWasatch = {
+    name: "Wasatch Peaks Organic",
+    brand: "Beans & Brew (High Altitude Roasting)",
+    roast: "Medium",
+    profile: ["well-rounded"],
+    origin: ["Unknown"],
     decaf: false,
 };
 
