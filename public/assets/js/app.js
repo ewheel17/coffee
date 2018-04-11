324bc50bd78e5e3abb54b8003c86c12f3ab8af72
Alec Down <alecdown@gmail.com>
2018-04-11 11:58:39 -0600

    - adds fade-in for dash to prevent unauth'd visits

---
+++ public/assets/js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("#dashboard-body").delay( 800 ).fadeIn( 400 );
+  $("#spinner").css("display","none");
+});
+
+$(document).ready(function(){
+  $("#landing-body").fadeIn(800);
+});
