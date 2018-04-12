90fdc2fb0cf1e55c33a43715665ab7ae379a9e4c
Max <ewheel17@gmail.com>
2018-04-12 16:52:54 -0600

    I have added miminal coffee shop 'search' functionality to the map.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -6,15 +6,45 @@ $(document).ready(function(){
   $("#landing-body").delay( 800 ).fadeIn( 400 );
 });
 
-// Creates a Google Map
+// Creates a Google Map centered in on SLC.
+var map;
+var infowindow;
+
 function initMap() {
-  var uluru = { lat: 40.76, lng: -111.891 };
-  var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 12,
-      center: uluru
+  var SLC = {lat: 40.765, lng: -111.841};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: SLC,
+    zoom: 12
   });
+
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: SLC,
+    radius: 4000,
+    keyword: ['coffee']
+  }, callback);
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+    console.log(results.length);
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
   var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
   });
 }
