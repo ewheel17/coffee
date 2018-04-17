e9ecf98271a6644716759340d67f679b14e7e826
Alec Down <alecdown@gmail.com>
2018-04-17 13:30:12 -0600

    adds server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+var app = express();
+
+app.set('view engine');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res) => {
+});
+
+app.get('/bad', (req,res) => {
+  res.send({
+      errorMessage: 'There was an error'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
