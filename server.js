d05e3bdb2d2e45814f29dbf43de0ef9767216163
Alec Down <alecdown@gmail.com>
2018-04-17 14:34:16 -0600

    adds dynamically generated port

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.get('/bad', (req,res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
